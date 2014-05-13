26f054d7cbc7bc46e1ff5fb16627d1a757029116
Stephen Helms <s.helms@amolf.nl>
2014-05-13 14:38:07 +0200

    Updated the example script and removed unnecessary test scripts in preparation for branching Jana's version to use for configuring the video files

--- python/example_script.py
+++ python/example_script.py
@@ -1,16 +1,21 @@
 # import worm tracker package
 import os
-os.chdir('//system-biologysrv.amolf.nl/users/Helms/Code/wormtracker-matlab')
+# Change this to the directory where the code is stored
+os.chdir('D://Documents/Stephen/Code/wormtracker-matlab')
 import wormtracker as wt
 
 # configure libav
 # wt.libavPath = C:\\libav\\bin
 
 # video settings
+# change this to the video folder
 os.chdir('//system-biologysrv.amolf.nl/users/Koers/worm_videos/day_7')
 videoFile = '2014-04-14_n2_a_b_day_7.avi'
 storeFile = '2014-04-14_n2_a_b_day_7_processed.h5'
+pickleFile = '2014-04-14_n2_a_b_day_7.dat'
 
+# if all are not from the same strain, change to allSameStrain=False
+# if number of regions < 16, add numberOfRegions=N
 wv = wt.WormVideo(videoFile, storeFile=storeFile,
                   allSameStrain=True)
 
@@ -26,9 +31,29 @@ wv.defineFoodRegions()
 # test video settings
 wv.testBackgroundFilter()
 wv.testThreshold()
-# was crashing on not finding the worm, maybe fixed now?
 wv.testWormIdentification()
 
 # process video
 wv.saveConfiguration()
+
+# save object to pickle file
+import cPickle
+with open(pickleFile, 'wb') as f:
+    cPickle.dump(wv, f)
+
+
+#
+# DONE LATER
+#
+import os
+# Change this to the directory where the code is stored
+os.chdir('D://Documents/Stephen/Code/wormtracker-matlab')
+import wormtracker as wt
+# load object
+import cPickle
+pickleFile = '2014-04-14_n2_a_b_day_7.dat'
+with open(pickleFile, 'rb') as f:
+    wv = cPickle.load(f)
+
+# run analysis
 wv.processRegions()  # analyzes each region serially
