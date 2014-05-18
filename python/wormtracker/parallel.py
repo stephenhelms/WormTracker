bbd73e767dee955eb444f297784a2e78d53c8d49
Stephen Helms <s.helms@amolf.nl>
2014-05-18 18:30:39 +0200

    Added some try/except blocks to try to avoid crashes. previous version of code appears to finish the analysis, but the threads don't return for some reason

--- python/wormtracker/parallel.py
+++ python/wormtracker/parallel.py
@@ -41,18 +41,26 @@ def parallelProcessRegions(wormVideo):
 def processRegion(region):
     print 'Starting analysis of {0} {1}'.format(region.strainName,
                                                 region.wormName)
-    # split output to a different file
-    path, name = os.path.split(region.resultsStoreFile)
-    newName = (region.strainName+'_'+region.wormName+'_'+name)
-    region.resultsStoreFile = os.path.join(path, newName)
-    tStart = time.clock()
-    region.saveConfiguration()
-    region.process()
-    tFinish = time.clock()
-    tDuration = (tFinish - tStart) / 60
-    print 'Analysis of {0} {1} took {2} min.'.format(region.strainName,
-                                                     region.wormName,
-                                                     str(tDuration))
+    try:
+        # split output to a different file
+        path, name = os.path.split(region.resultsStoreFile)
+        newName = (region.strainName+'_'+region.wormName+'_'+name)
+        region.resultsStoreFile = os.path.join(path, newName)
+        tStart = time.clock()
+        region.saveConfiguration()
+        region.process()
+        tFinish = time.clock()
+        tDuration = (tFinish - tStart) / 60
+        print 'Analysis of {0} {1} took {2} min.'.format(region.strainName,
+                                                         region.wormName,
+                                                         str(tDuration))
+        return 'Success'
+    except(Exception) as e:
+        print 'Error during analysis of {0}{1}: {2}'.format(region.strainName,
+                                                            region.wormName,
+                                                            str(e))
+        return 'Failed'
+
 
 def cleanUpPostProcess(wormVideo):
     # merge results into original output file
