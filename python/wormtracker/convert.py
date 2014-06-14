e314ce84996fe02142b5732b5566f14631a9ba1a
Stephen Helms <stephen.helms@gmail.com>
2014-06-13 22:43:38 -0400

    convert -- Started writing module for importing/exporting from other worm datasets

---
+++ python/wormtracker/convert.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+import numpy.ma as ma
+import h5py
+from scipy.io import loadmat
+
+
+def trajEnsFromStephensRyu(filename):
+    mat = loadmat(filename)
+    raise NotImplemented()
