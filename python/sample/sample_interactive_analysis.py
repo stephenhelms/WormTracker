34666a058b48972a53332b9b31245e2ee3a6d498
Stephen Helms <stephen.helms@gmail.com>
2014-08-22 20:20:48 +0200

    Updated interactive analysis script

--- python/sample/sample_interactive_analysis.py
+++ python/sample/sample_interactive_analysis.py
@@ -13,12 +13,17 @@ with open(configFile, 'r') as f:
 wv = wvs[0]
 storeFile = wv.storeFile
 
+# manual load .h5 file
+storeFile = '/Users/stephenhelms/Dropbox/OpenData/short_test.h5'
+
 # load results
 import h5py
 import wormtracker.analysis as wta
 wta.configureMatplotLibStyle()  # make prettier plots
-f = h5py.File(storeFile, 'r')
+f = h5py.File(storeFile, 'r')  # data is only loaded in memory as needed (good for handling large ensembles)
 strain = 'N2'
+
+# INDIVIDUAL TRAJECTORIES
 worm = '1'
 traj = wta.WormTrajectory(f, strain, worm)
 traj.plotTrajectory()
@@ -34,7 +39,7 @@ plt.show()
 traj.plotSpeed()
 
 # plot speed distribution
-traj.plotSpeedDistribution(bins=np.linspace(0,500,100))
+traj.plotSpeedDistribution()
 
 # plot mean-squared displacmeent
 # (statistical description of position over time)
@@ -73,3 +78,70 @@ traj.plotPosturalTimeSeries(0)
 
 # plot postural state space -- first and second posture
 traj.plotPosturalPhaseSpace(0,1)
+
+# in 3D
+traj.plotPosturalPhaseSpace3D(0,1,2)
+
+# as density
+traj.plotPosturalPhaseSpaceDensity(0,1)
+
+# ENSEMBLES
+# make an ensemble of all the worms (6 in this case)
+ens = wta.WormTrajectoryEnsemble([])
+for worm in f['worms'][strain]:
+	ens.append(wta.WormTrajectory(f, strain, worm))
+ens = wta.WormTrajectoryEnsemble([traj for traj in ens if ~traj.allCentroidMissing and ~traj.allPostureMissing])
+ens.sort()
+
+# tile trajectory plots (can modify to do the same for any trajectory object plot)
+ens.tilePlots(lambda t: t.plotTrajectory(showPlot=False))
+
+# plot the mean squared displacement
+ens.plotMeanSquaredDisplacement()
+
+# plot the speed distribution
+ens.plotSpeedDistribution()
+
+# calculate postural measurements from the ensemble
+ens.calculatePosturalMeasurements()
+
+# plot the postural covariance
+ens.plotPosturalCovariance()
+
+# plot the postural mode distribution
+ens.plotPosturalModeDistribution()
+
+# plot the postural phase space density (need to update to use KDE)
+ens.plotPosturalPhaseSpaceDensity(0,1)
+
+# GROUPS
+# divide the data into two ensembles
+ens1 = ens[:3]
+ens1.name = '0-2'
+ens2 = ens[3:]
+ens2.name = '3-5'
+colorDict = {ens1.name: 'k', ens2.name: 'r'}
+group = wta.WormTrajectoryEnsembleGroup([ens1, ens2], colorScheme=lambda e: colorDict[e.name])
+
+# plot mean squared displacement
+group.plotMeanSquaredDisplacement()
+
+# plot speed distributions
+group.plotSpeedDistribution()
+
+
+# MODELS (only for single trajectories for now)
+import wormtracker.models as wtm
+m = wtm.Helms2014CentroidModel()  # Helms 2014 model
+
+# fit reversal component
+m.fitReversals(traj, plotFit=True)
+
+# fit bearing drift
+m.fitBearingDrift(traj, plotFit=True)
+
+# fit bearing diffusion
+m.fitBearingDiffusion(traj, plotFit=True)
+
+# fit speed component
+m.fitSpeed(traj, plotFit=True)
